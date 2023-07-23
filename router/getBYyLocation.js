05e1b5723e8e1594a7349319174a8a40305c43c0
Manoj9599 <70386832+Manoj9599@users.noreply.github.com>
2023-07-23 11:45:48 +0530

    Update getBYyLocation.js- added optional chaining

--- router/getBYyLocation.js
+++ router/getBYyLocation.js
@@ -3,9 +3,10 @@ const data = require("./data.json");
 module.exports = getBYyLocation = async (req, res) => {
   try {
     const { assetName, location } = req.params;
-
+      console.log(assetName,'assetName')
+    console.log(location,'location')
     const foundObjects = data[assetName].filter(
-      (obj) => obj.Location.trim() === location.trim()
+      (obj) => obj?.Location?.toLowerCase().trim() === location?.toLowerCase().trim()
     );
 
     return res.status(200).send(foundObjects);
